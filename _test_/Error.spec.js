fe3b5497cb2a62237dd149612511b77bfa8884fd
Femi Balogun <phembarl@yahoo.com>
2019-07-31 11:50:52 +0100

    chore(react): set up empty react project - create new project - set up and configure Webpack - setup and configure eslint using Airbnb linting option - [Finishes #166840962]

---
+++ _test_/Error.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+
+import Error from '../src/views/Error';
+
+describe('App', () => {
+  let app;
+  beforeEach(() => {
+    app = shallow(<Error />);
+  });
+
+  it('renders successfully', () => {
+    expect(app).toBeDefined();
+  });
+
+  it('renders a div component', () => {
+    expect(app.find('div').length).toBe(1);
+  });
+
+  it('renders a h1 component', () => {
+    expect(app.find('h1').length).toBe(1);
+  });
+
+  it('renders a h3 component', () => {
+    expect(app.find('h3').length).toBe(1);
+  });
+
+  it('renders a ul component', () => {
+    expect(app.find('ul').length).toBe(1);
+  });
+  it('renders a li component', () => {
+    expect(app.find('li').length).toBe(1);
+  });
+  it('renders a Link component', () => {
+    expect(app.find('Link').length).toBe(1);
+  });
+});
